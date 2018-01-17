e7fcf5869da3411a1b4b91e7ea31f0d501088855
Murithi Borona <borona.murithi@radix.co.ke>
2018-01-17 18:02:38 +0300

    Updated storage status mountpoint to /storage/data

--- api/local_api/apiv1/utils.py
+++ api/local_api/apiv1/utils.py
@@ -7,18 +7,18 @@ Utilities for interacting with the filesystem.
 import os
 
 
-def get_storage_status(path='/'):
+def get_storage_status(mount_point='/storage/data'):
     """Returns the disk storage status of a BRCK device in bytes.
+
+    The default mountpoint `/storage/data`.
 
     Assumes that everything is mounted under /
     :ret dict
     """
     stats = os.statvfs(path)
     state = dict(
-        total_space = stats.f_blocks * stats.f_bsize,
-        used_space = stats.f_bfree * stats.f_frsize,
-        available_space = stats.f_bavail * stats.f_bsize
+        total_space=stats.f_blocks * stats.f_frsize,
+        used_space=(stats.f_blocks - stats.f_bfree) * stats.f_frsize,
+        available_space=stats.f_bavail * stats.f_frsize
     )
     return state
-
-
