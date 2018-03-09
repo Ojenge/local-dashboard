70d711f884de8f220fc21f179ded4f5f087056f3
Murithi Borona <murithi@brck.com>
2018-03-09 15:39:08 +0300

    Bugfix - history upgrade introduces unusual redirect behaviour (redirects to browser start page).

--- brck-local-dashboard/src/components/Boot.js
+++ brck-local-dashboard/src/components/Boot.js
@@ -55,9 +55,6 @@ class Boot extends Component {
   }
 
   render() {
-    if (this.state.connected && this.props.history.length > 0) {
-      this.props.history.goBack();
-    }
     return (
       this.state.connected
         ? <Redirect to='/dashboard' />
